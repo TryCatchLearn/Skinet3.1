6aa43bebe00b55e20983e377ae33c653b7199cf8
trycatchlearn <trycatchlearn@outlook.com>
2020-02-17 18:28:14 +0700

    Order cascade fix

--- Infrastructure/Data/Config/OrderConfiguration.cs
+++ Infrastructure/Data/Config/OrderConfiguration.cs
@@ -18,6 +18,8 @@ namespace Infrastructure.Data.Config
                     o => o.ToString(),
                     o => (OrderStatus) Enum.Parse(typeof(OrderStatus), o)
                 );
+
+            builder.HasMany(o => o.OrderItems).WithOne().OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
